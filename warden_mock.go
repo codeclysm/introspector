c3f0cc9d64e3a9c175772eb200a02bcc3ff876e1
Luca Osti <luca.d.osti@gmail.com>
2017-12-05 12:33:07 +0100

    Provide useable token format

--- warden_mock.go
+++ warden_mock.go
@@ -1,6 +1,7 @@
 package introspector
 
 import (
+	"encoding/json"
 	"errors"
 	"strings"
 )
@@ -10,25 +11,29 @@ import (
 type WardenMock struct {
 }
 
+// holds the grants for a user, kinda like a ladon policy
+type mockGrants struct {
+	Actions   []string          `json:"actions"`
+	Context   map[string]string `json:"context"`
+	Resources []string          `json:"resources"`
+	Scopes    []string          `json:"scopes"`
+	Subject   string            `json:"subject"`
+}
+
 // Introspect accepts a token in this form:
-//   "subject.scope1,scope2.action1,action2.resource1,resource2.context:value,context2:value"
+// `{"subject":"123456","scopes":["profile:cars","car:seats"],"actions":["drive","sell"],"resources":["car:777"],"context":{"weather":"windy"}}`
 // and will return an appropriate introspection
 func (m WardenMock) Introspect(token string) (*Introspection, error) {
 	introspection := new(Introspection)
 
-	parts := strings.Split(token, ".")
-	if len(parts) < 5 {
-		return nil, errors.New("The token must be in the form 'subject.scope1,scope2.action1,action2.resource1,resource2.context:value,context2:value'")
+	var grants mockGrants
+	if err := json.Unmarshal([]byte(token), &grants); err != nil {
+		return nil, errors.New(`The token is malformed, should look like {"subject":"123456","scopes":["profile:cars","car:seats"],"actions":["drive","sell"],"resources":["car:777"],"context":{"weather":"windy"}}`)
 	}
 
-	introspection.Subject = parts[0]
+	introspection.Subject = grants.Subject
 
-	introspection.Scope = strings.Join(strings.Split(parts[1], ","), " ")
-
-	introspection.Extra = map[string]interface{}{}
-	introspection.Extra["actions"] = strings.Split(parts[2], ",")
-	introspection.Extra["resources"] = strings.Split(parts[3], ",")
-	introspection.Extra["context"] = strings.Split(parts[4], ",")
+	introspection.Scope = strings.Join(grants.Scopes, " ")
 
 	introspection.Active = true
 
@@ -36,49 +41,52 @@ func (m WardenMock) Introspect(token string) (*Introspection, error) {
 }
 
 // Allowed accepts a token in this form:
-//   "subject.scope1,scope2.action1,action2.resource1,resource2.context:value,context2:value"
+//  `{"subject":"subject","scopes":["scope1","scope2"],"actions":["action1","action2"],"resources":["resource1","resource2"],"context":{"context":"value","context2":"value2"}}`
+//
+// This form is kinda the same form as the JSON-encoded ladon policies.
 //
 // scope1 and scope2 are granted oauth2 scopes
 // action1 and action2 are some actions the subject can perform on the resources
 // resource1 and resource2 are some resource the subject has access to
 // context:value and context2:value2 are some context values
 //
-// Example: "123456.profile:public.drive,sell.car.owner_id:123456"
+// Example: `{"subject":"123456","scopes":["profile:cars","car:seats"],"actions":["drive","sell"],"resources":["car:777"],"context":{"weather":"windy"}}`
 func (m WardenMock) Allowed(token string, perm Permission, scopes ...string) (i *Introspection, can bool, err error) {
 	i, err = m.Introspect(token)
 	if err != nil {
 		return i, false, err
 	}
 
+	var grants mockGrants
+	if err := json.Unmarshal([]byte(token), &grants); err != nil {
+		return nil, false, errors.New(`The token is malformed, should look like {"subject":"123456","scopes":["profile:cars","car:seats"],"actions":["drive","sell"],"resources":["car:777"],"context":{"weather":"windy"}}`)
+	}
+
 	// Check scopes
 	for _, scope := range scopes {
-		if !in(strings.Split(i.Scope, " "), scope) {
+		if !in(grants.Scopes, scope) {
 			return i, false, nil
 		}
 	}
 
 	// Check resource
-	if res, ok := i.Extra["resources"]; ok {
-		resources := res.([]string)
-		if !in(resources, perm.Resource) {
+	if perm.Resource != "" {
+		if !in(grants.Resources, perm.Resource) {
 			return i, false, nil
 		}
 	}
 
 	// Check action
-	if res, ok := i.Extra["actions"]; ok {
-		actions := res.([]string)
-		if !in(actions, perm.Action) {
+	if perm.Action != "" {
+		if !in(grants.Actions, perm.Action) {
 			return i, false, nil
 		}
 	}
 
 	// Check context
-	if res, ok := i.Extra["context"]; ok {
-		for key, value := range perm.Context {
-			if !in(res.([]string), key+":"+value) {
-				return i, false, nil
-			}
+	for key, value := range perm.Context {
+		if res, ok := grants.Context[key]; !ok || res != value {
+			return i, false, nil
 		}
 	}
 
--- warden_mock_test.go
+++ warden_mock_test.go
@@ -15,7 +15,7 @@ func TestWardenMock(t *testing.T) {
 		Expected         bool
 		ExpIntrospection introspector.Introspection
 	}{
-		{"123456.profile:cars,car:seats.drive,sell.car:777,car:779.dayofweek:friday,weather:windy",
+		{`{"subject":"123456","scopes":["profile:cars","car:seats"],"actions":["drive","sell"],"resources":["car:777"],"context":{"dayofweek":"friday","weather":"windy"}}`,
 			introspector.Permission{
 				Action:   "drive",
 				Resource: "car:777",
@@ -26,28 +26,18 @@ func TestWardenMock(t *testing.T) {
 			introspector.Introspection{
 				Active:  true,
 				Subject: "123456",
-				Scope:   "profile:cars car:seats",
-				Extra: map[string]interface{}{
-					"resources": []string{"car:777", "car:779"},
-					"actions":   []string{"drive", "sell"},
-					"context":   []string{"dayofweek:friday", "weather:windy"},
-				}}},
+				Scope:   "profile:cars car:seats"}},
 		// Only check scopes
-		{"123456.profile:cars...",
+		{`{"subject":"123456","scopes":["profile:cars"]}`,
 			introspector.Permission{},
 			[]string{"profile:cars"},
 			true,
 			introspector.Introspection{
 				Active:  true,
 				Subject: "123456",
-				Scope:   "profile:cars",
-				Extra: map[string]interface{}{
-					"resources": []string{""},
-					"actions":   []string{""},
-					"context":   []string{""},
-				}}},
+				Scope:   "profile:cars"}},
 		// Wrong context
-		{"123456.profile:cars,car:seats.drive,sell.car:777,car:779.dayofweek:friday,weather:windy",
+		{`{"subject":"123456","scopes":["profile:cars","car:seats"],"actions":["drive","sell"],"resources":["car:777"],"context":{"dayofweek":"friday","weather":"windy"}}`,
 			introspector.Permission{
 				Action:   "drive",
 				Resource: "car:777",
@@ -58,14 +48,9 @@ func TestWardenMock(t *testing.T) {
 			introspector.Introspection{
 				Active:  true,
 				Subject: "123456",
-				Scope:   "profile:cars car:seats",
-				Extra: map[string]interface{}{
-					"resources": []string{"car:777", "car:779"},
-					"actions":   []string{"drive", "sell"},
-					"context":   []string{"dayofweek:friday", "weather:windy"},
-				}}},
+				Scope:   "profile:cars car:seats"}},
 		// Missing resource
-		{"123456.profile:cars,car:seats.drive,sell.car:779.dayofweek:friday,weather:windy",
+		{`{"subject":"123456","scopes":["profile:cars","car:seats"],"actions":["drive","sell"],"resources":["car:779"],"context":{"weather":"windy"}}`,
 			introspector.Permission{
 				Action:   "drive",
 				Resource: "car:777",
@@ -75,14 +60,9 @@ func TestWardenMock(t *testing.T) {
 			introspector.Introspection{
 				Active:  true,
 				Subject: "123456",
-				Scope:   "profile:cars car:seats",
-				Extra: map[string]interface{}{
-					"resources": []string{"car:779"},
-					"actions":   []string{"drive", "sell"},
-					"context":   []string{"dayofweek:friday", "weather:windy"},
-				}}},
+				Scope:   "profile:cars car:seats"}},
 		// Missing action
-		{"123456.profile:cars,car:seats.drive,sell.car:777.dayofweek:friday,weather:windy",
+		{`{"subject":"123456","scopes":["profile:cars","car:seats"],"actions":["drive","sell"],"resources":["car:777"],"context":{"weather":"windy"}}`,
 			introspector.Permission{
 				Action:   "destroy",
 				Resource: "car:777",
@@ -92,14 +72,9 @@ func TestWardenMock(t *testing.T) {
 			introspector.Introspection{
 				Active:  true,
 				Subject: "123456",
-				Scope:   "profile:cars car:seats",
-				Extra: map[string]interface{}{
-					"resources": []string{"car:777"},
-					"actions":   []string{"drive", "sell"},
-					"context":   []string{"dayofweek:friday", "weather:windy"},
-				}}},
+				Scope:   "profile:cars car:seats"}},
 		// Missing scope
-		{"123456.profile:cars.drive,sell.car:777,car:779.dayofweek:friday,weather:windy",
+		{`{"subject":"123456","scopes":["profile:cars"],"actions":["drive"],"resources":["car:777"],"context":{"weather":"windy"}}`,
 			introspector.Permission{
 				Action:   "drive",
 				Resource: "car:777",
@@ -110,12 +85,7 @@ func TestWardenMock(t *testing.T) {
 			introspector.Introspection{
 				Active:  true,
 				Subject: "123456",
-				Scope:   "profile:cars",
-				Extra: map[string]interface{}{
-					"resources": []string{"car:777", "car:779"},
-					"actions":   []string{"drive", "sell"},
-					"context":   []string{"dayofweek:friday", "weather:windy"},
-				}}},
+				Scope:   "profile:cars"}},
 	}
 	for _, tc := range cases {
 		mock := introspector.WardenMock{}
